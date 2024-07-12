71b9febfe14f6a2a33cde1a26975918fe2c9427a
Francesco Donnini <donnini.francesco00@gmail.com>
2024-07-12 16:03:59 +0200

    aggiunta euristica aprtf

--- heuristics.py
+++ heuristics.py
@@ -1,8 +1,65 @@
 import sys
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Any
 
+import objective
 from job import Job
 from slice import JobSlice
+
+
+def aprtf(jobs: list[Job]) -> list[JobSlice]:
+    unscheduled: set[Job] = set(jobs)
+    schedule: list[JobSlice] = []
+    t: int = -sys.maxsize
+    while len(unscheduled) > 0:
+        a: Job = min_prio(unscheduled, t)
+        b: Job = min_release(unscheduled, t)
+        x: Job
+        if a.release_date <= ri(b, t):
+            x = a
+        else:
+            mu: int = len(unscheduled) - 2
+            tau: int = smallest_release_date(unscheduled.difference({a, b}))
+            if f(a, b, t) - f(b, a, t) < mu * min(ri(a, t) - ri(b, t), ei(b, ei(a, t)) - tau):
+                x = b
+            else:
+                x = a
+        schedule.append(JobSlice(x, max(t, x.release_date), x.duration))
+        t = ei(x, t)
+        unscheduled.remove(x)
+    return schedule
+
+
+def smallest_release_date(jobs: Iterable[Job]) -> int:
+    m: Job = min(jobs, key=lambda x: x.release_date, default=None)
+    if m is None:
+        return 0
+    else:
+        return m.release_date
+
+
+def min_prio(jobs: Iterable[Job], t: int) -> Job:
+    minimums: list[Job] = abstract_min(jobs, key=lambda x: prio_rule(x, t))
+    if len(minimums) == 1:
+        return minimums[0]
+    minimums = abstract_min(minimums, key=lambda x: ri(x, t))
+    if len(minimums) == 1:
+        return minimums[0]
+    return min(minimums, key=lambda x: int(x.identifier))
+
+
+def min_release(jobs: Iterable[Job], t: int) -> Job:
+    minimums: list[Job] = abstract_min(jobs, key=lambda x: ri(x, t))
+    if len(minimums) == 1:
+        return minimums[0]
+    minimums = abstract_min(minimums, key=lambda x: x.duration)
+    if len(minimums) == 1:
+        return minimums[0]
+    return min(minimums, key=lambda x: int(x.identifier))
+
+
+def abstract_min(jobs: Iterable[Job], key: Callable[[Job], Any]) -> list[Job]:
+    m: Job = min(jobs, key=lambda x: key(x))
+    return list(filter(lambda x: key(x) == key(m), jobs))
 
 
 def prtf(jobs: list[Job]) -> list[JobSlice]:
@@ -45,9 +102,19 @@ def prio_rule(x: Job, t: int) -> int:
     return 2 * max(t, x.release_date) + x.duration
 
 
+def ei(j: Job, t: int) -> int:
+    return ri(j, t) + j.duration
+
+
 def ri(j: Job, t: int) -> int:
     return max(j.release_date, t)
 
 
 def ci(j: Job, t: int) -> int:
     return max(j.release_date, t) + j.duration
+
+
+def f(a: Job, b: Job, t: int) -> int:
+    s1 = JobSlice(a, max(a.release_date, t), a.duration)
+    schedule = [s1, JobSlice(b, max(s1.completion_time(), b.release_date), b.duration)]
+    return objective.total_completion_time(schedule)
